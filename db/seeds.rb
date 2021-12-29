57d497547e16becbce1571c1089d7a12d92df1d0
soc-man <okoth.sc@gmail.com>
2021-12-29 19:18:54 +0300

    added first product to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+niceCat = Product.create(title: "Quality building view", 
+    subtitle: "Sam", image_name: "Building.JPG",
+    price: "4.99", sku: "BuildingOne", info: "nice building", download_url: "https://rails-e-commerce.s3.us-east-2.amazonaws.com/header-image.jpg", 
+    details: "A really nice building", description: %{<p>It's a building</p>
+    })
